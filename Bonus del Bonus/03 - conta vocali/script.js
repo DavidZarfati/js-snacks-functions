a740712f2258dd0b1c003f967ba8289fac78a0cf
DavidZarfati <zarda2001@gmail.com>
2025-11-04 14:50:55 +0100

    feat:esercizio 3 con arrow

--- Bonus del Bonus/03 - conta vocali/script.js
+++ Bonus del Bonus/03 - conta vocali/script.js
@@ -4,18 +4,34 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-function vocali(word) {
-    let arrayVocali = []
-    for (i = 0; i < word.length; i++) {
-        if (word.charAt(i) === "a" || word.charAt(i) === "e" || word.charAt(i) === "i" || word.charAt(i) === "o" || word.charAt(i) === "u") {
-            arrayVocali.push(word.charAt(i))
-        }
-    }
-    return arrayVocali
-}
+// function vocali(word) {
+//     let arrayVocali = []
+//     for (i = 0; i < word.length; i++) {
+//         if (word.charAt(i) === "a" || word.charAt(i) === "e" || word.charAt(i) === "i" || word.charAt(i) === "o" || word.charAt(i) === "u") {
+//             arrayVocali.push(word.charAt(i))
+//         }
+//     }
+//     return arrayVocali
+// }
 
 // Invoca la funzione qui e stampa il risultato in console
-console.log(vocali(word))
+// console.log(vocali(word))
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+
+// NON FUNZIONANTE
+// const vocali = (word, vocali) => {
+//     return word.filter(vocali => "aeiou".includes(vocali))
+// }
+// console.log(vocali(word))
+
+// TENTATIVO 2
+
+const vocali = (word) => {
+    return word
+        .split('')
+        .filter(lettera => 'aeiou'.includes(lettera))
+}
+
+console.log(vocali(word))
