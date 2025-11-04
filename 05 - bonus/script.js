328fb29e92e211ae9e36ea49272ff450408c664a
DavidZarfati <zarda2001@gmail.com>
2025-11-04 16:03:55 +0100

    feat: gioco con il codice,aggiunte simpatiche

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -12,15 +12,15 @@ const name = 'Mario';
 function saluto(name, ora) {
     let messaggio = ""
     if (ora >= 0 && ora <= 13) {
-        messaggio = "Buongiorno " + name
+        messaggio = "Buongiorno " + name + "🌅"
     }
     else if (ora >= 13 && ora <= 17) {
-        messaggio = "Buon pomeriggio " + name
+        messaggio = "Buon pomeriggio " + name + " è ora di pranzo" + "🍣"
     }
     else if (ora >= 17 && ora <= 24) {
-        messaggio = "Buonasera " + name
+        messaggio = "Buonasera " + name + " 🌆"
     }
-    else { messaggio = ("Non hai inserito un orario corretto") }
+    else { messaggio = "Non hai inserito un orario corretto 🙅🏻" }
     return messaggio
 }
 
@@ -32,3 +32,23 @@ console.log(saluto("Mario", 37))
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
+
+
+// e se fosse Natale?
+console.log(`hey Loris non è natale🎅🏻, ma...🎃 Buone feste, è un piacere averti qui,🦈 goditi questo albero🎄`)
+let alberoUtente = prompt("dimmi un numero da a 10")
+function alberoDiNatale(altezza) {
+
+    for (let i = 1; i <= altezza; i++) {
+        let spazi = "  ".repeat(altezza - i);
+        let stelle = "🎄".repeat(i * 2 - 1);
+        console.log(spazi + stelle);
+    }
+
+    let troncoSpazi = " ".repeat(altezza - 1);
+    console.log(troncoSpazi + "       |    ");
+}
+
+
+alberoDiNatale(alberoUtente);
+console.log(`Hai scelto di avere un albero alto ${alberoUtente}`)
