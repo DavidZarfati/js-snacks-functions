fae4cb038065578c271042611668a1e49185b6a4
DavidZarfati <zarda2001@gmail.com>
2025-11-04 14:42:22 +0100

    feat:fine esercizio 2 con arrow function

--- Bonus del Bonus/02 - Filtra iniziali/script.js
+++ Bonus del Bonus/02 - Filtra iniziali/script.js
@@ -4,20 +4,27 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-function iniziali(names, lettera) {
-    let primaLetteraUguale = ""
-    let arrayInizialiUguali = []
-    for (let i = 0; i < names.length; i++) {
-        primaLetteraUguale = names[i].charAt(0)
-        if (primaLetteraUguale === lettera) {
-            arrayInizialiUguali.push(names[i])
-        }
-    }
-    return arrayInizialiUguali;
-}
+// function iniziali(names, lettera) {
+//     let primaLetteraUguale = ""
+//     let arrayInizialiUguali = []
+//     for (let i = 0; i < names.length; i++) {
+//         primaLetteraUguale = names[i].charAt(0)
+//         if (primaLetteraUguale === lettera) {
+//             arrayInizialiUguali.push(names[i])
+//         }
+//     }
+//     return arrayInizialiUguali;
+// }
 
 // Invoca la funzione qui e stampa il risultato in console
-console.log(iniziali(names, "A"))
+// console.log(iniziali(names, "A"))
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
+
+const iniziali = (names, lettera) => {
+    return names
+        .map(name => name.charAt(0))
+        .filter(iniziale => iniziale === lettera)
+}
+console.log(iniziali(names, "A"))
