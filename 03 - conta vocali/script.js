22e83fbaf98f698d44c2559fefde47d09f1c6f4d
DavidZarfati <zarda2001@gmail.com>
2025-11-04 14:16:21 +0100

    feat:fine esercizio 3

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -4,10 +4,18 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+function vocali(word) {
+    let arrayVocali = []
+    for (i = 0; i < word.length; i++) {
+        if (word.charAt(i) === "a" || word.charAt(i) === "e" || word.charAt(i) === "i" || word.charAt(i) === "o" || word.charAt(i) === "u") {
+            arrayVocali.push(word.charAt(i))
+        }
+    }
+    return arrayVocali
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(vocali(word))
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
